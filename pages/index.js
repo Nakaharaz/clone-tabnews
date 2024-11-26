f6a1d477b1ca71e7e1b265b1375479a73061239f
Enzo Nakahara <103426974+Nakaharaz@users.noreply.github.com>
2024-11-26 12:48:50 +0000

    Changing the text on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amorecuda, eu te amo demaisssssss!!!!! E se vc me ama dá uma risadola! 😼😻😹</h1>
+    return <h1>Amorecuda, eu te amo demaisssssss!!!!! Eu espero que tenha dado tudo certo no seu TCC e vc tenha tirado MB!! E se vc me ama dá uma risadola! 😼😻😹</h1>
 }
 
 export default Home
