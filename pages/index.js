c741f1277421ba57a5a9c1126d205b69c45f6ca1
Enzo Nakahara <103426974+Nakaharaz@users.noreply.github.com>
2024-11-26 12:42:14 +0000

    Add emojis at pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amorecuda, eu te amo demaisssssss!!!!! E se vc me ama dá uma risadola!</h1>
+    return <h1>Amorecuda, eu te amo demaisssssss!!!!! E se vc me ama dá uma risadola! 😼😻😹</h1>
 }
 
 export default Home
