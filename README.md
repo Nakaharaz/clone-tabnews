803b69228fb1573515fcba62bf2e98437525bdf5
Enzo Nakahara <103426974+Nakaharaz@users.noreply.github.com>
2024-11-28 11:02:02 +0000

    Add the scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 My clone of TabNews to curso.dev. Used to enhance my knowledge
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Amorecuda, eu te amo demaisssssss!!!!! Eu espero que tenha dado tudo certo no seu TCC e vc tenha tirado MB!! E se vc me ama dá uma risadola! 😼😻😹</h1>
+  return (
+    <h1>
+      Amorecuda, eu te amo demaisssssss!!!!! Eu espero que tenha dado tudo certo
+      no seu TCC e vc tenha tirado MB!! E se vc me ama dá uma risadola! 😼😻😹
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
