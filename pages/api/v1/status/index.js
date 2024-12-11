7851fec5a07beb48dfbb69564599437276abdd24
Enzo Nakahara <japanakah@outlook.com>
2024-12-11 01:02:07 -0300

    Finish the `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,30 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "O Enzo é uma pessoa acima da média!" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1  ;",
+    values: [databaseName],
+  });
+  const openedConnections = openedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        db_version: dbVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,25 @@
 test("GET  to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
+  // Check if /status is accessible
   expect(response.status).toBe(200);
+
+  // Check if updated_at is defined
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  // Check if response updated_at is a valid and equal to parsed date
+  const pasedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(pasedUpdatedAt);
+
+  // Check if Postgres version is 16.0
+  expect(responseBody.dependecies.database.db_version).toBe("16.0");
+
+  // Check if Max connections is <= to 100
+  expect(responseBody.dependecies.database.max_connections).toBeLessThanOrEqual(
+    100,
+  );
+
+  // Check if Opened connections is <= to Max connections
+  expect(responseBody.dependecies.database.opened_connections).toBe(1);
 });
