6ffc33bffe41630c6d4b79a36886179b81d09af8
Enzo Nakahara <japanakah@outlook.com>
2025-02-19 07:24:44 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,38 +6,49 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        // Check if /migrations is accessible
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+        // Check if migrations run and return is the number of runned migrations
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThanOrEqual(1);
+
+        // Check if database live table was affected
+        const dbMigrations = (
+          await database.query("SELECT * FROM pgmigrations;")
+        ).rows;
+        expect(dbMigrations.length).toBeGreaterThanOrEqual(1);
+      });
+      test("For the second time", async () => {
+        // Second fetch to check if all the migrations has been runned
+        const secondResponse = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        // Check if /migrations still accessible
+        expect(secondResponse.status).toBe(200);
+
+        const secondResponseBody = await secondResponse.json();
+
+        // Check if number of migrations is 0
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
+    });
   });
-
-  // Check if /migrations is accessible
-  expect(response.status).toBe(201);
-
-  const responseBody = await response.json();
-  // Check if migrations run and return is the number of runned migrations
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThanOrEqual(1);
-
-  // Check if database live table was affected
-  const dbMigrations = (await database.query("SELECT * FROM pgmigrations;"))
-    .rows;
-  expect(dbMigrations.length).toBeGreaterThanOrEqual(1);
-
-  // Second fetch to check if all the migrations has been runned
-  const secondResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-
-  // Check if /migrations still accessible
-  expect(secondResponse.status).toBe(200);
-
-  const secondResponseBody = await secondResponse.json();
-
-  // Check if number of migrations is 0
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
 });
